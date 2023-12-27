d21efbdbd8f064d59d51cbc549922f10d5fccc11
UnknownReason0 <142212484+UnknownReason0@users.noreply.github.com>
2023-12-27 22:32:11 +0530

    Update password.py

--- plugins/ExtraMods/password.py
+++ plugins/ExtraMods/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/mkn_bots_updates')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('🎁 Create Own Bots', url='https://t.me/Bot_Making_Tips')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
